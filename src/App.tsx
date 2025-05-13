7622c2fa4cdca8ce9f1967853e9235fe63899079
Nicolas GIRARD <NG38@outlook.fr>
2025-05-13 09:55:05 +0200

    création des liens (non-actif) stylisé et placé

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,24 @@
 import './App.css'
+import Link from "./components/Link.tsx";
+import Header from "./components/Header.tsx";
+import Footer from "./components/Footer.tsx";
 
 function App() {
 
 
   return (
-    <>
+      <>
+        <header className={"headerClass"}>
+          <Header />
+        </header>
+        <main>
+          <Link />
+        </main>
+        <footer className={"footerClass"}>
+          <Footer />
+        </footer>
+      </>
 
-    </>
   )
 }
 
---
+++ src/components/Header.tsx
@@ -0,0 +1,13 @@
+import '../App.css'
+
+function Header() {
+    return(
+        <>
+            <section className={"headerSection"}>
+                <img src={"src/assets/Logo_dark.png"} alt={"logo"} className={"logo"} />
+            </section>
+        </>
+    )
+}
+
+export default Header;
