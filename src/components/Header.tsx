dd24c8d4217f1f5b8a2919f9f2713b1dcb3567d5
Nicolas GIRARD <53898852+NicolasG38@users.noreply.github.com>
2025-05-13 12:14:50 +0200

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,7 @@ function Header() {
     return(
         <>
             <section className={"headerSection"}>
-                <img src={"src/assets/Logo_dark.png"} alt={"logo"} className={"logo"} />
+                <img src={"./src/assets/Logo_dark.png"} alt={"logo"} className={"logo"} />
             </section>
         </>
     )
